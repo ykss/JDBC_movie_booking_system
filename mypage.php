3059ece029ae442eb90a7eb29cadc45ab0c39ac1
System Administrator <root@cha-eunbiui-MacBook-Pro.local>
2018-05-20 23:36:28 +0900

    complete reservation history

--- mypage.php
+++ mypage.php
@@ -20,6 +20,16 @@ include "comment.inc.php";
     background-color: rgba(255,255,255,0.7);
     text-align: center;
   }
+  table {
+    width: 800px;
+    border: 2px solid black;
+    background-color: rgba(255,255,255,0.7);
+  }
+  th, td {
+    width:200px;
+
+    text-align: center;
+  }
 </style>
 <body>
 
@@ -51,25 +61,38 @@ include "comment.inc.php";
       개인정보수정</button></center>
     </form>";
     ?>
-    <center><h3>Reservation List</h3></center>
+    <center><h3 style="background-color:rgba(255,255,255,0.7);width:300px">Reservation List</h3></center>
     <?php
-      $rlt = mysqli_query($conn,"SELECT reserve_date, title, date FROM ReservationList JOIN MovieScheduleList
+      $rlt = mysqli_query($conn,"SELECT reserve_id, reserve_date, title, date FROM ReservationList JOIN MovieScheduleList
                               ON ReservationList.customer_id=$currentId AND ReservationList.schedule_id = MovieScheduleList.schedule_id
                               JOIN MovieInfoList ON MovieScheduleList.movie_id = MovieInfoList.movie_id");
-      $List = mysqli_fetch_array($rlt); ?>
-      <div id='info'>
-        <?php
-      echo "예약일시: ";
-      echo $List['reserve_date'];
-      echo "<br>";
-      echo "영화제목: ";
-      echo $List['title'];
-      echo "<br>";
-      echo "상영일시: ";
-      echo $List['date'];
-      echo "<br>";
-     ?>
+    ?>
 
-    </div>
+      <center><table>
+        <tr>
+          <th>Title</th>
+          <th>Date</th>
+          <th># of People</th>
+          <th>Reservation Date</th>
+        </tr>
+      </table></center>
+      <center><table>
+      <?php while($List = mysqli_fetch_array($rlt)){
+        $id = $List['reserve_id'];
+        $rlt2 = mysqli_query($conn, "SELECT ticket_id FROM TicketList WHERE TicketList.reserve_id='$id'");
+        $number = 0;
+        while($ticket = mysqli_fetch_array($rlt2)){
+          $number++;
+        }
+        ?>
+        <tr>
+          <td><?php echo $List['title'];?></td>
+          <td><?php echo $List['date'];?></td>
+          <td><?php echo $number; ?></td>
+          <td><?php echo $List['reserve_date'];?></td>
+        </tr>
+  <?php } ?>
+</table>
+</center>
 </body>
 </html>
